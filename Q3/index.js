1a1298f13aad11ccb506cc530ba5eb766369933b
Ace-kv <talhashahid100@yahoo.com>
2023-12-22 17:50:44 +0500

    Remove JS files from tracking

--- Q3/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// Write a program that uses a for loop to print the first 25 integers.
-for (let i = 0; i < 25; i++) {
-    console.log(i);
-}
--- Q4/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// Write a program that uses a for loop to print the first 10 even numbers.
-for (let i = 0; i < 25; i++) {
-    if (i % 2 === 0) {
-        console.log(i);
-    }
-}
